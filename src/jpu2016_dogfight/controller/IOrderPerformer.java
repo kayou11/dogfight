a4064419b4aeabcab388e51e612735674c34b7c2
kayou11 <p.kerebelomer@gmail.com>
2016-05-24 09:41:49 +0200

    Phase 2

--- src/jpu2016_dogfight/controller/IOrderPerformer.java
+++ src/jpu2016_dogfight/controller/IOrderPerformer.java
@@ -1,1 +1,8 @@
 package jpu2016_dogfight.controller;
+
+
+
+public interface IOrderPerformer{
+	
+	public void orderPerform(UserOder userOrder);
+}
--- src/jpu2016_dogfight/controller/IUserOrder.java
+++ src/jpu2016_dogfight/controller/IUserOrder.java
@@ -1,1 +1,7 @@
 package jpu2016_dogfight.controller;
+
+public interface IUserOrder{
+	
+	public Order getOrder();
+	public int getPlayer();
+}
